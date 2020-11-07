1026d533475166d675d30a968157f8f1ddb9f45c
Josh Wulf <josh@magikcraft.io>
2020-11-08 03:47:38 +1300

    Fix
    Signed-off-by: Josh Wulf <josh@magikcraft.io>

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t sitapati/slack-archivist
+docker build -t sitapati/slack-archivist .
 docker-compose -f deploy/docker-compose.yml down
 docker-compose -f deploy/docker-compose.yml up -d
 docker-compose -f deploy/docker-compose.yml logs -f
