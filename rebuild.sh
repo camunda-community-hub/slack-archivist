bc9e578512d737161761fb640d78cf4a4901e8f1
Josh Wulf <josh@magikcraft.io>
2020-11-18 22:45:37 +1300

    Increase timeout

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export COMPOSE_HTTP_TIMEOUT=120
+
 docker build -t sitapati/slack-archivist .
 docker-compose -f deploy/docker-compose.yml down
 docker-compose -f deploy/docker-compose.yml up -d
