2c43dd0d471c0f4f448479f54c0555693683dcb2
Josh Wulf <josh@magikcraft.io>
2020-11-18 23:31:45 +1300

    verbose

--- rebuild.sh
+++ rebuild.sh
@@ -3,4 +3,4 @@
 docker build -t sitapati/slack-archivist .
 docker-compose -f deploy/docker-compose.yml down
 docker-compose -f deploy/docker-compose.yml up -d
-COMPOSE_HTTP_TIMEOUT=120 docker-compose -f deploy/docker-compose.yml logs -f
+COMPOSE_HTTP_TIMEOUT=120 docker-compose --verbose -f deploy/docker-compose.yml logs -f
