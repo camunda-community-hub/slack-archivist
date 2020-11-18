272d2e9f61758ab577dd8e537f3cbdcf8498cb61
Josh Wulf <josh@magikcraft.io>
2020-11-18 22:11:26 +1300

    Colorize logs

--- src/PostBuilder.ts
+++ src/PostBuilder.ts
@@ -90,26 +90,6 @@ export class PostBuilder {
   threadMessages(messages: SlackMessageEvent[]) {
     // Somehow it looks like the thread is now threaded already
     return messages;
-
-    // const messageIsThreadParent = (event: SlackMessageEvent) =>
-    //   event.thread_ts === event.ts;
-
-    // const threadParentMessage = messages.filter(messageIsThreadParent)[0];
-
-    // // Reorder the message according to the threading metadata. They are not ordered in the array.
-    // const orderedRepliesIndex =
-    //   threadParentMessage.replies?.map((reply) => reply.ts) || []; // Allows a single post to be archived by calling the bot in the first thread message
-    // const messageThread = [threadParentMessage];
-    // orderedRepliesIndex.forEach((replyts) => {
-    //   const reply = messages.filter((msg) => msg.ts == replyts);
-    //   if (reply.length === 1) {
-    //     messageThread.push(reply[0]);
-    //   }
-    // });
-    // debug("messageThread:");
-    // debug(messageThread);
-
-    // return messageThread;
   }
 
   replaceUsercodesWithNames(
