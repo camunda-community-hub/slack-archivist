240dc18500b4f9884cea94b33425eeeaab0f94fc
Josh Wulf <josh@magikcraft.io>
2022-05-03 09:59:18 +1200

    Don't use cache on docker rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t sitapati/slack-archivist .
+docker build -t sitapati/slack-archivist . --no-cache
 docker-compose -f deploy/docker-compose.yml down
 docker-compose -f deploy/docker-compose.yml up -d
 COMPOSE_HTTP_TIMEOUT=120000 docker-compose --verbose -f deploy/docker-compose.yml logs -f
